60c41b2f07da514d436a5095b356c679ba5a5437
Sivan Kaplan <sivanagar@gmail.com>
2022-01-02 14:50:01 -0800

    shopping cart

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -7,10 +7,11 @@ const { Provider } = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
     products: [],
+    cart: [],
+    cartOpen: false,
     categories: [],
     currentCategory: '',
   });
-  console.log(state);
 
   return <Provider value={[state, dispatch]} {...props} />;
 };
